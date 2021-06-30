b7bd557dc581c73800ee4357c1f94baca12ffe4e
w0en <owenlee1998@gmail.com>
2021-06-30 17:10:02 +0800

    Base 1x1 cube frame drawn and rendered

--- rubikscube.py
+++ rubikscube.py
@@ -5,13 +5,13 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 vertices = (
-    (1, -1, -1),
+    (-1, 1, 1),
+    (1, 1, 1),
     (1, 1, -1),
     (-1, 1, -1),
     (-1, -1, -1),
+    (1, -1, -1),
     (1, -1, 1),
-    (1, 1, 1),
-    (-1, 1, 1),
     (-1, -1, 1)
 )
 
@@ -29,3 +29,42 @@ edges = (
     (5, 6),
     (6, 7)
 )
+
+def Cube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+
+
+
+
+
+
+
+
+            glVertex3fv(vertices[vertex])
+
+    glEnd()
+
+
+def main():
+    pygame.init()
+    DISPLAY = (800, 600)
+    pygame.display.set_mode(DISPLAY, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (DISPLAY[0]/DISPLAY[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+    glRotatef(0, 0, 0, 0)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+main()
